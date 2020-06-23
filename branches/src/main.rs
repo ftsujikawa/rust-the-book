f6c127326c8a47c176b717628c55e444cbb2fc4d
ftsujikawa <readme627@gmail.com>
2020-06-24 08:37:50 +0900

    第３章終了

---
+++ branches/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let number = 3;
+
+    // if number < 5 {
+    //     println!("condition was true");
+    // }
+    // else {
+    //     println!("condition was false");
+    // }
+    if number {
+        println!("number was three");
+    }
+}
---
+++ loops/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // loop {
+    //     println!("again!");
+    // }
+    // for number in (1..4).rev() {
+    //     println!("{}!", number);
+    // }
+    // println!("LIFTOFF!!!");
+    for number in 1..4 {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+}
