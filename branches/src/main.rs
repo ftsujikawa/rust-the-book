29be8eefb0476e6d317200d7a178205859e405ac
Fumihito Tsujikawa <readme627@gmail.com>
2021-01-27 20:33:08 +0900

    第３章終了

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,13 +1,25 @@
 fn main() {
-    let number = 3;
-
-    // if number < 5 {
-    //     println!("condition was true");
-    // }
-    // else {
-    //     println!("condition was false");
-    // }
-    if number {
-        println!("number was three");
+   /*  let number = 3;
+ */
+    /* if number < 5 {
+        println!("condition was true");
     }
+    else {
+        println!("condition was false");
+    } */
+/*     if number {
+        println!("number was three");
+    } */
+    /* if number != 0 {
+        println!("number was something other than zero");
+    } */
+    let condition = true;
+    let number = if condition {
+        5
+    }
+    else {
+        6
+    };
+
+    println!("The value of number is: {}", number);
 }
--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,4 +1,4 @@
-fn main() {
+/* fn main() {
     println!("Hello, world!");
 
     another_function();
@@ -6,4 +6,11 @@ fn main() {
 
 fn another_function() {
     println!("Another fuction.");
+} */
+fn main() {
+    anather_function(5,6);
+}
+fn anather_function(x: i32, y: i32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
 }
