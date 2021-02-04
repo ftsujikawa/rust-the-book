340b1431dc673bbfcd1b34140e724a33871254aa
Fumihito Tsujikawa <readme627@gmail.com>
2021-02-04 15:34:16 +0900

    第９章終了

---
+++ ch09/p176/src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+use std::io::Read;
+use std::fs::File;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn main() {
+    match read_username_from_file() {
+        Ok(s) => println!("s is {}", s),
+        Err(e) => panic!("Error: {:?}", e),
+    };
+}
