e294963911a163c80c6477cb7d0cb5ed0e5662b2
Fumihito Tsujikawa <readme627@gmail.com>
2021-02-09 16:09:46 +0900

    第１３章終了

---
+++ ch13/list13-1/src/main.rs
@@ -0,0 +1,13 @@
+use std::thread;
+use std::time::Duration;
+
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
+}
+
+fn main() {
+    let s = simulated_expensive_calculation(3);
+    println!("s is {}", s);
+}
